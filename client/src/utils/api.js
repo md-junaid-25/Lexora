d8894607ccafa732d449b2025f0475a5e14e2dcb
Mohammed Junaid <mdjunaid9aug@gmail.com>
2026-01-18 15:39:05 +0530

    Update API base URL to production server

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your Node server URL
+  baseURL: 'https://lexora-otlc.onrender.com/api', // Node server URL
 });
 
 // Automatically add the Token to every request if it exists
